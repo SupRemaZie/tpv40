fbdb932d6908697f0030717f841391080d5c1abd
Damien <Damien.porchet.Etu@univ-lemans.fr>
2023-03-28 19:38:23 +0200

    mis à jour du panier auto

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,22 @@
-function updateShoppingCart(){
-    var panier = document.forms['panier'];
+function updateShoppingCart() {
+
     $.ajax({
-        body: '/recalculerPanier',
-        data: $(panier).serialize(),
-        type: 'GET',
-        success: function(data){
-            console.log(data);
+        url: $('form').attr('action'), // Récupère l'URL de l'action du formulaire
+        method: $('form').attr('method'), // Récupère la méthode HTTP du formulaire (GET ou POST)
+        data: $('form').serialize(), // Récupère les données du formulaire sous forme de chaîne de requête
+        success: function(response) {
+            // Code à exécuter en cas de succès de la requête Ajax
+           // met à jour le panier
+              document.querySelector('body').innerHTML = response;
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Code à exécuter en cas d'erreur de la requête Ajax
+            console.log(textStatus + ": " + errorThrown);
         }
     });
 }
+
+
+
+
